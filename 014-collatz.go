6aedb05ca4011972daeb5380c173a23c7abafe7a
Leo Nistor <leo.nistor@gmail.com>
2013-02-19 21:28:04 +0200

    unhappy 14 solution

--- 014-collatz.go
+++ 014-collatz.go
@@ -29,38 +29,63 @@ import (
 	"fmt"
 )
 
-func main() {
-	const stopNum int = 1000000 // one million
-	var theNumber, theSteps int
+const stopNum int = 1000000 // one million
+var theNumber, theSteps int
+var stepMap map[int]int // a map with the number of Collatz steps for each number
 
-	for i := 13; i < stopNum; i++ {
-		fmt.Print(".")
-		s := stepsCollatz(i)
-		if s > theSteps {
-			theSteps = s
+func main() {
+
+	stepMap = make(map[int]int) // Maps must be created with make (not new) before use
+
+	stepMap[1] = 3
+	stepMap[2] = 1
+	stepMap[3] = 7
+	stepMap[4] = 2
+	stepMap[5] = 5
+	stepMap[6] = 8
+
+	for i := 7; i < stopNum; i++ {
+
+		steps := 0
+		n := i
+		found := false
+		// fmt.Println("--- pentru ", i)
+
+		for n != 1 && !found {
+			n = nextCollatz(n)
+			// fmt.Print(n, ">")
+			steps++
+			if v, ok := stepMap[n]; ok {
+				steps = steps + v
+				found = true
+				// fmt.Print("stop at", n, "it has", v)
+			}
+		}
+		stepMap[i] = steps
+
+		if steps >= theSteps {
+			theSteps = steps
 			theNumber = i
-			fmt.Println(theSteps, "steps for", theNumber)
+			fmt.Println(i, "has", steps, "steps -------------------")
+		}
+		if i == 837799 {
+			fmt.Println(i, "has", steps, "steps ***********")
 		}
 	}
 
-	fmt.Println(theSteps, "steps for", theNumber)
-
+	// print final result
+	fmt.Println("FINALLY: ", theSteps, "steps for", theNumber)
 }
 
 func nextCollatz(n int) int {
-	if n%2 == 0 {
-		return n / 2
+	if n > 1 {
+		if n%2 == 0 {
+			return n / 2
+		} else {
+			return 3*n + 1
+		}
 	} else {
-		return 3*n + 1
+		return 1
 	}
 	return 1
-}
-
-func stepsCollatz(n int) int {
-	i := 0
-	for n != 1 {
-		n = nextCollatz(n)
-		i++
-	}
-	return i
 }
